3ff4a2af77d3d221d57c66271ce406e1d457ba06
Jessiecodes <Jessak1202@gmail.com>
2020-06-16 09:54:46 -0400

    Updates

---
+++ precache-manifest.949f1505d2c1f43b35e033eb16178e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5015fd16ab4fa840c3fca760026cd5",
+    "url": "/ReactPixaBayAPI/index.html"
+  },
+  {
+    "revision": "3003a163a3170867d66d",
+    "url": "/ReactPixaBayAPI/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bc89aee2366a69948e28",
+    "url": "/ReactPixaBayAPI/static/js/2.9c46813c.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/ReactPixaBayAPI/static/js/2.9c46813c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3003a163a3170867d66d",
+    "url": "/ReactPixaBayAPI/static/js/main.31198f97.chunk.js"
+  },
+  {
+    "revision": "53af49bac377baaabb63",
+    "url": "/ReactPixaBayAPI/static/js/runtime-main.e307f132.js"
+  }
+]);
